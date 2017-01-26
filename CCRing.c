8c1378899e85e7ab64576c2f7b3e6f6e8276bcf0
CatherineZeng <yczeng@mit.edu>
2017-01-26 11:12:32 -0500

    added ccAppend

--- CCRing.c
+++ CCRing.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h> // memset
 #include "CCRing.h"
+#include <stdio.h>
 
 
 CCRing* createRing(unsigned long length) {
@@ -30,4 +31,28 @@ CCError freeRing(CCRing* pRing) {
   free(pRing->data);
   free(pRing);
   return ccNoError;
+}
+
+/*comments to help me think
+given length inputed 
+length modulo length of ring <- which index of the ring you want to modify to be */
+
+int ccAppend(CCRing* appendRing, ccAudioDataType array[], unsigned long length) {
+  
+      int index = appendRing->index_ring;
+
+  for (int i = 0; i < length; ++i) {
+      int n = (index + i) % appendRing->length;
+      appendRing->data[n] = array[i];
+
+      if (i==(length - 1) ){
+        index = ((index + i + 1) % appendRing->length);
+        appendRing->index_ring = index;
+      }
+
+      
+    }
+
+
+  return 1;
 }
